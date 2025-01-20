22a96463cfb860b746e32593f075a43c0ce5e6ab
chaeyoo <chaeh0124@gmail.com>
2025-01-20 22:02:01 +0900

    style: tab, border 컬러 변경

--- src/components/tabs/index.tsx
+++ src/components/tabs/index.tsx
@@ -8,11 +8,11 @@ export default function Tabs({ tabList }: { tabList: ITabsProps[] }) {
 
   return (
     <>
-      <div className="flex items-center w-full border bg-pf-gray text-pf-white rounded-md">
+      <div className="flex items-center w-full bg-pf-gray text-pf-white rounded-md">
         {tabList.map((v) => (
           <div
             key={v.idx}
-            className={`flex-1 py-1 text-center cursor-pointer  ${v.idx === selected.idx ? 'bg-pf-darknavy font-semibold' : 'bg-pf-gray hover:bg-pf-darknavy hover:bg-opacity-60'} rounded-md m-1`}
+            className={`flex-1 py-1 text-center cursor-pointer  ${v.idx === selected.idx ? 'bg-pf-purple font-semibold' : 'bg-pf-gray hover:bg-pf-darknavy hover:bg-opacity-60'} rounded-md m-1`}
             onClick={() => {
               setSelected(v)
             }}
@@ -21,7 +21,7 @@ export default function Tabs({ tabList }: { tabList: ITabsProps[] }) {
           </div>
         ))}
       </div>
-      <div className=" px-5 py-7 border border-pf-gray rounded-md mt-2">
+      <div className=" px-5 py-7 border border-pf-purple rounded-md mt-2">
         {selected.content}
       </div>
     </>
