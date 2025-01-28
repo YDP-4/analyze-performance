8c0235e4487bd6ddcb8f4d4c57b18bdc570840fe
LeeWxx <dnckd0903@naver.com>
2025-01-28 21:11:35 +0900

    feat: 네비게이션 타이밍 데이터 수집 API 추가

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,9 +1,33 @@
 export interface AnalyzeResult {
   domGraph: DomNode;
+  navigationTiming: NavigationTiming;
 }
 
 export type DomNode = {
-    id: string;
-    tag: string;
-    children: DomNode[];
-  };
+  id: string;
+  tag: string;
+  children: DomNode[];
+};
+
+
+export interface NavigationTiming {
+  type: string;
+  startTime: number;
+  duration: number;
+  redirectCount: number;
+  redirectStart: number;
+  redirectEnd: number;
+  domainLookupStart: number;
+  domainLookupEnd: number;
+  connectStart: number;
+  connectEnd: number;
+  secureConnectionStart: number;
+  requestStart: number;
+  responseStart: number;
+  responseEnd: number;
+  domInteractive: number;
+  domContentLoadedEventStart: number;
+  domContentLoadedEventEnd: number;
+  loadEventStart: number;
+  loadEventEnd: number;
+}
